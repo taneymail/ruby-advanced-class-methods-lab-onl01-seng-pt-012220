e7aabbd54282d0ae6bf47e8bc9905a1aeddb89ba
T S <taneymail@gmail.com>
2020-02-15 21:20:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,3 @@
-# class Song
-#  attr_accessor :name, :artist_name
- # @@all = []
-
-#  def self.all
- #   @@all
- # end
-
-#  def save
-#    self.class.all << self
- # end
-
-#end
-
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -84,3 +70,4 @@ end
     @@all = Array.new
   end
 end
+
